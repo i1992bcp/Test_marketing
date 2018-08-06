b7ce2ff18939b1db7c95c508e71b320cf155bd93
hitarth <hshah@bestchoiceproducts.com>
2018-08-06 16:08:25 -0700

    change packege

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require("express");
 var app = express();
-
+const PORT = process.env.PORT
 
 app.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+app.listen(PORT);
